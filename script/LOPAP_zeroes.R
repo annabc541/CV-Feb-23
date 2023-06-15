ddd76320ab7173006e00d9c5adc56ca82a0bc7b1
annabc541 <abc541@york.ac.uk>
2023-06-15 10:14:27 +0100

    update

--- script/LOPAP_zeroes.R
+++ script/LOPAP_zeroes.R
@@ -5,14 +5,28 @@ library(lubridate)
 
 Sys.setenv(TZ = "UTC")
 
-raw_dat = read.csv("data/raw_data/reagents1.csv") %>% 
+raw_dat = read.csv("data/raw_data/reagents0.5.csv") %>% 
   mutate(date = glue::glue("{Date} {Time}"),
-       date = ymd_hms(date)) %>% 
+       date = dmy_hms(date)) %>% 
   arrange(date) %>% 
-  select(date,ch1 = X550.6,ch2 = X550)
+  select(date,ch1,ch2)
+
+cal = raw_dat %>% 
+  filter(date > "2023-02-09 12:30" & date < "2023-02-09 12:55")
+
+cal %>% 
+  pivot_longer(c(ch1,ch2)) %>%
+  ggplot(aes(date,value)) +
+  geom_point() +
+  facet_grid(rows = vars(name),scales = "free_y") +
+  scale_x_datetime(date_breaks = "5 min",date_labels = "%H:%M") +
+  NULL
+
+
+
 
 zero = raw_dat %>% 
-  filter(date > "2023-02-17 04:10" & date < "2023-02-17 04:18")
+  filter(date > "2023-02-09 05:20" & date < "2023-02-09 06:10")
 
 zero %>% 
   pivot_longer(c(ch1,ch2)) %>%
@@ -25,5 +39,5 @@ zero %>%
 initial_value = zero$ch1[1]
 ten_percent = initial_value*10/100
 min_value = min(zero$ch1)
-final_value = zero$ch1[120]
+final_value = zero$ch1[90]
 ninty_percent = final_value*90/100
